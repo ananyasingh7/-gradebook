b99fd48328a27fc15cd0542a85a9a967565a02c2
Ananya Singh <as2863@njit.edu>
2018-12-23 21:22:32 -0500

    updating objects

--- main.java
+++ main.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
+
 public class main{
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("Testing Again");
-    System.out.println("tmux test");
+    Scanner input = new Scanner(System.in);
+
+    String username;
+    String password;
+
+    System.out.println("WELCOME TO THE GRADEBOOK: ");
+    System.out.println("--------------------------");
+    System.out.println("Please Login");
+    System.out.println("Username: ");
+    username = input.next();
+
+    System.out.println("Password: ");
+    password = input.next();
+    input.close();
+    System.out.println(username + " " + password);
+
+
+
+
+
   }
+
+
+
+
 }
